07b895250c04ba71bef331dc96baff72e01d9a03
Ewarm <seamanwarm@sina.com>
2019-03-23 18:51:28 +0800

    导航守卫/mate/

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router) 
 /* layout */
-import Layout from "../views/layout/layout.vue"
+import Layout from "../views/layout/index.vue"
+import dashboard from "../views/dashboard/index.vue"
  
 /* 路由懒加载 */
 /* 路由懒加载有三种方法 */
@@ -21,18 +22,7 @@ import Layout from "../views/layout/layout.vue"
 ] */
 /* 不需要权限的路由 */
  const constantRoutes=[
-    {
-        path:"/redirect",
-        component: Layout,
-        hidden:true,
-        children:[
-            {
-                path:"/redirect/:path*",
-                components:()=>import("../views/redirect/index.vue")
-            }
-        ]
-        
-    },
+  
     {
         path:"/login",
         name:"Login",
@@ -41,7 +31,34 @@ import Layout from "../views/layout/layout.vue"
     {
         path:"/",
         redirect: "/dashboard",
-        component:()=>import("../views/dashboard/index.vue")
+        component:Layout,
+        children:[
+            {path:"dashboard",component:dashboard}
+        ],
+         
+
+    },
+
+]
+/* 导航守卫 */
+/* mate 是路由元信息
+   作用: 携带信息,所有的路由都会有路由记录最终形成$route中的 $route.matched数组，可以通过循环的方式 
+   找找到，结合导航守卫可以决定渲染的页面是什么，或者下面进行何等操作
+   $route.matched.some(record=>record.meta.requiresAuth) 
+   some 
+   1. 找到则为 ture 
+   2. 不会对空数组进行循环
+   3. 找到一个则不会再继续往下找，如果找不到则为false
+    
+ */
+const asynRouterMap=[
+    {path:"/permission",
+     component:Layout,
+     name:"权限测试" ,
+     mate:{role:["admin","super_editor"]}, //即页面需要的权限要求
+     children:[
+        {path:"index",}
+     ]
 
     }
 ]
---
+++ src/views/layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class=''> 
+        <p>这是整个的</p>
+        <router-view></router-view>
+    </div>
+    
+</template>
+<script>
+export default {
+    name:"layouts"
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
+
