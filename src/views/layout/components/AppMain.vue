2866fdf36f5600f1466c4f09a582df31cf8e5b4b
Ewarm <seamanwarm@sina.com>
2019-03-23 18:02:03 +0800

    面板构建

---
+++ src/views/layout/components/AppMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="dashboard-container">
+        <p>这是面板</p>
+       <!--  <component :is='currentRole' /> -->
+    </div>
+</template>
+<script>
+export default {
+    name:"Dashboard",
+   /*  data(){
+        return{
+          currentRole:"adminDashboard"  
+        }
+    } */
+}
+</script>
+<style>
+    
+</style>
+
+
