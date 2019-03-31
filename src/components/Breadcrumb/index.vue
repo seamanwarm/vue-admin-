55678ab3b890b3e63fa6e0cb126c0ec8647ba7e2
Ewarm <seamanwarm@sina.com>
2019-04-01 00:25:22 +0800

    面包屑中菜单展开收起处理

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <p>导航</p>
+    <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    </el-breadcrumb>
 </template>
 <script>
 export default {
